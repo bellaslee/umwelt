b13ad10f6d8c57c927c4b10df9fe847ea6b507a4
bella <bella@bellalee.com>
2023-10-15 11:32:52 -0700

    Add link to source code for mobile app

--- src/pages/index.js
+++ src/pages/index.js
@@ -101,6 +101,7 @@ export default function Home() {
 
       <Section>
         <h2>...with a functional prototype</h2>
+        <p><a href="https://github.com/minorenji/Dubhacks23" target="_blank" rel="noopener noreferrer">View source code here</a></p>
         <iframe width="100%" height="800" src="https://www.youtube.com/embed/rS5eb6bbxHg?si=vtKqiCmc56Lyan_J" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </Section>
 
