cc8c9e17d64cd1bc93e914cffa2fb61cf85e0efd
bella <bella@bellalee.com>
2023-10-15 11:26:34 -0700

    Add team member links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer({ font }) {
   return (
     <footer className={`${font.className} container text-center`}>
-      &copy; Annie Pao, Bella Lee, Sean Lim, and Stephanie Chou for Dubhacks 2023
+      &copy; <a href="https://www.linkedin.com/in/an-yao-pao-6992a1217" target="_blank" rel="noopener noreferrer">Annie Pao</a>, <a href="https://bellalee.com" target="_blank" rel="noopener noreferrer">Bella Lee</a>, <a href="https://minorenji.github.io/" target="_blank" rel="noopener noreferrer">Sean Lim</a>, and <a href="https://www.linkedin.com/in/stephanie-chou-4066a51b9/" target="_blank" rel="noopener noreferrer">Stephanie Chou</a> for Dubhacks 2023
     </footer>
   )
 }
